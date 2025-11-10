c0d9350abfc79ad9c7efd878b6f95941d09040b7
RishigheshG <grishighesh@gmail.com>
2025-11-10 18:36:47 +0100

    Game Completed!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Number Guessing Game using Python 
 ### This is my 1st Project which is part of my 100 Projects in 100 Days series
 
-Just a normal number guessing game
+Just a normal number guessing game which using the random package
---
+++ number-guessing-game.py
@@ -0,0 +1,31 @@
+import random
+
+# randrange generates a random number from 0 to 11 exclusive and randint generates a random number from 0 to 11 inclusive
+
+top_of_range = input("Type a number: ")
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+r = random.randint(0, top_of_range)
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == r:
+        print("You got it!")
+        break
+    elif user_guess > r:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
